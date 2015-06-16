b229eaff4fb57e12187912821bf79fc042441492
Jorge Hernandez <jorge_l_hernandez@yahoo.com>
2015-06-16 19:17:19 -0400

    Messed around with Gulp, got the grid working and refactored

--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,55 @@
 var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass = require('gulp-sass');
 
-gulp.task('do-something', function() {
+gulp.task('browser-sync', function() {
 
-
-
-
-  console.log(arguments);
-  console.log('I did something');
-})
-
-gulp.task('sass', function() {
-  var sass = require('gulp-sass');
-  gulp.src('src/scss/main.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('src/css/main.css'))
-    .pipe(sass({
-      outputStyle: 'compressed'
-    }))
-    .pipe(gulp.dest('dist/css/'))
-}); //End of gulp.task(sass)
-gulp.watch('watch:sass', function() {
-
-  gulp.watch('src/scss/*.scss', ['sass'], function() {
-    console.log('File changed');
-  });
-  gulp.watch('src/index.html', ['build']);
-
-})
+      browserSync.init({
+        server: {
+          baseDir: "./"
+        }
+      });
+    } //End of gulp.task browser-sync
 
 
-gulp.task('build', ['sass'], function() {
 
-  //! means no scss files
-  gulp.src(['src/*', '!src/scss']) //this is basically (gulpfrom())
+    gulp.task('serve', ['sass'], function() {
 
-  .pipe(gulp.dest('dist/')); //.dest refers to the destination. This is basically(gulp .into)
-  //We are piping to a destination
+      browserSync.init({
+        server: "src/"
+      });
 
-});
+      gulp.watch('src/scss/*.scss', ['sass']);
+      gulp.watch('src/index.html').on('change', browserSync.reload);
+
+    });
+
+    gulp.task('sass', function() {
+      var sass = require('gulp-sass');
+      gulp.src('src/scss/main.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('src/css/main.css'))
+        .pipe(sass({
+          outputStyle: 'compressed'
+        }))
+        .pipe(gulp.dest('dist/css/'))
+    }); //End of gulp.task(sass)
+    // gulp.watch('watch:sass', function() {
+    //
+    //   gulp.watch('src/scss/*.scss', ['sass'], function() {
+    //     console.log('File changed');
+    //   });
+    //   gulp.watch('src/index.html', ['build']);
+    //
+    // })
+    //
+    //
+    // gulp.task('build', ['sass'], function() {
+    //
+    //   //! means no scss files
+    //   gulp.src(['src/*', '!src/scss']) //this is basically (gulpfrom())
+    //
+    //   .pipe(gulp.dest('dist/')); //.dest refers to the destination. This is basically(gulp .into)
+    //   //We are piping to a destination
+    //
+    // });
